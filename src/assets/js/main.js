50147242fce28e440cdf146a1e4bdb9d9d50a7f4
IgorSavchenko <ig0r@ua.fm>
2018-07-19 15:02:43 +0300

    Init

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  var msnry = new Masonry( '.content__collage__gallery', {
+    // options
+    transitionDuration: '0.2s',
+    horizontalOrder: true
+  });
+
+})
