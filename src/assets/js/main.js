7b281d0a7a40645ac5e7377694254cb2b408de28
IgorSavchenko <ig0r@ua.fm>
2018-07-19 15:26:38 +0300

    DomContentLoaded event changed to load event

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
 
+  console.log("All resources finished loading!");
   var msnry = new Masonry( '.content__collage__gallery', {
     // options
     transitionDuration: '0.2s',
